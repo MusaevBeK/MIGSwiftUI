8bbfb18ce5b041621e24c540acc4cb1c3fddced9
MusaevBeK <beksultan1923@gmail.com>
2022-08-22 11:23:42 +0600

    Initial Commit

---
+++ MIGSwiftUI/MIGSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIGSwiftUIApp.swift
+//  MIGSwiftUI
+//
+//  Created by user on 22/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MIGSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
